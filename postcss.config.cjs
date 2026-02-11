5d9ed3b1e46dc0aced9fc8b7d2f0a75ffd37d20d
Daniel L <daniel.leonl@upb.edu.co>
2026-02-11 15:11:24 -0500

    Fix PurgeCSS plugin import

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,12 @@
 const purgecss = require("@fullhuman/postcss-purgecss");
+const purgecssPlugin = purgecss.default ?? purgecss;
 
 const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
   plugins: isProduction
     ? [
-        purgecss({
+        purgecssPlugin({
           content: ["./index.html", "./src/**/*.{ts,tsx}"] ,
           safelist: ["hidden", "hovered", "paused"],
           defaultExtractor: (content) => content.match(/[A-Za-z0-9_-]+/g) || []
